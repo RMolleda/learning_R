b00fa31acba6cbb4b7e30dcab29853fc3c658e45
RMolleda <molledajr@gmail.com>
2021-02-15 17:49:42 +0100

    "First commit of exercises"

---
+++ Exercise1/vectores1.R
@@ -0,0 +1,51 @@
+# Ejercicios Vector
+
+# Ejercicio 3:
+
+x <- vector()
+y <- c(4,2,3,5,6)
+for (i in 1:length(y))
+  x <- c(x, y[i])
+print(x)
+
+# Ejercicio 6
+
+print(c("Sum: ",sum(x), "Sum: ", mean(x), "Product: ", prod(x)))
+
+# Ejercicio 7
+
+x <- c(10, 15, NaN, 13)
+sum(x, na.rm = TRUE)
+mean(x, na.rm = TRUE)
+prod(x, na.rm = TRUE)
+
+# Ejercicio 8
+
+min(x)
+max(x)
+
+# Ejercicio 9 
+
+sort(x)
+sort(x, decreasing = TRUE)
+
+# Ejercicio 10 
+
+is.na(x)
+is.element(15, x)
+
+# Ejercicio 12
+
+tail(x, 1)
+
+# Ejercicio 13
+
+sort(x, partial = length(x)-1)[-2:-1]
+
+# Ejercicio 14
+
+for (i in 1:length(x))
+  y=sort(x)[i]
+  print(y)
+
+# Ejercicio 16 
